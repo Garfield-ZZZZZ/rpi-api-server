86c8c61a20046d2a16b47970844714a864024d0f
Garfield <12821862+Garfield-ZZZZZ@users.noreply.github.com>
2022-01-30 10:33:48 +0800

    updated build script to include date in build tag

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-tag="garfieldz.azurecr.io/rpi-api-server:`git rev-parse HEAD | head -c 7`"
+tag="garfieldz.azurecr.io/rpi-api-server:`date +%F`_`git rev-parse HEAD | head -c 7`"
 docker build -t $tag .
